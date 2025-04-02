506571d4b448c9b086355db833b94049c0cbea12
OlubodeMichael <144721035+OlubodeMichael@users.noreply.github.com>
2025-04-02 17:27:21 -0400

    Update page.js

--- frontend/app/shipments/page.js
+++ frontend/app/shipments/page.js
@@ -291,7 +291,7 @@ export default function Dashboard() {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredShipments.map((shipment) => (
-              <ShipmentCard shipment={shipment} />
+              <ShipmentCard shipment={shipment} key={shipment._id || index} />
             ))}
           </div>
         )}
